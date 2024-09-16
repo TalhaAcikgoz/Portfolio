dd4cac8f8b36ff46a1af2b739ce5563c26842a0a
TalhaAcikgoz <89697506+TalhaAcikgoz@users.noreply.github.com>
2024-09-16 19:39:45 +0300

    init

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+  return (
+    <>
+      <div className="px-2 py-2">
+        <div className="flex items-center justify-center text-center px-7 py-7 bg-gray-100 rounded-lg">
+          <p className="w-full">
+          © 2023 TalhaAcikgoz – React Template by <a href="https://github.com/TalhaAcikgoz" className="font-medium underline" >Talha</a>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
